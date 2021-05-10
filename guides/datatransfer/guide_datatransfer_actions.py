8a92acd98ee33af391debfbc257974cc2cb0cdec
jpbouza <jpbouza@hotmail.com>
2021-05-10 00:32:46 -0300

    Add Modifiers on Finish Step

--- guides/datatransfer/guide_datatransfer_actions.py
+++ guides/datatransfer/guide_datatransfer_actions.py
@@ -37,6 +37,23 @@ def select_armature(operator, context):
     set_active_object(context, operator.arm_obj)
     set_mode('POSE')
 
+def check_mod_type(mod_type):
+    active = bpy.context.active_object
+    if hasattr(active, 'modifiers'):
+        for mod in active.modifiers:
+            if hasattr(mod, 'type'):
+                if mod.type == mod_type:
+                    return True
+
+def check_mod_type_name(mod_type, mod_name):
+    active = bpy.context.active_object
+    if hasattr(active, 'modifiers'):
+        for mod in active.modifiers:
+            if hasattr(mod, 'type'):
+                if mod.type == mod_type:
+                    if mod.name == mod_name:
+                        return True
+
 #### DATATRANSFER STEPS ####
 
 def DT_Weight_Mesh_Shapekey_Head(operator, context):
@@ -147,6 +164,9 @@ def DT_Edit_Head(operator, context):
     mdef_weights_model_objects = collect_mdef_weights_model()
     collect_mdef_weights_model()
     blenrig_temp_link(mdef_weights_model_objects)
+    for ob in mdef_weights_model_objects:
+        bpy.context.scene.blenrig_guide.mdef_weights_transfer_obj = ob
+        bpy.context.scene.blenrig_guide.mdef_weights_transfer_obj.hide_viewport = False
 
     set_mode('EDIT')
 
@@ -200,6 +220,9 @@ def DT_Edit_Hands(operator, context):
     mdef_weights_model_objects = collect_mdef_weights_model()
     collect_mdef_weights_model()
     blenrig_temp_link(mdef_weights_model_objects)
+    for ob in mdef_weights_model_objects:
+        bpy.context.scene.blenrig_guide.mdef_weights_transfer_obj = ob
+        bpy.context.scene.blenrig_guide.mdef_weights_transfer_obj.hide_viewport = False
 
     set_mode('EDIT')
 
@@ -219,9 +242,163 @@ def DT_Finish(operator, context):
     # Hide MDefWeightsModel
     blenrig_temp_unlink()
 
-    bpy.context.scene.blenrig_guide.arm_obj.hide_viewport = False
+    #Turn off Tranfer Shapekeys on Character's Objects
+    try:
+        bpy.context.scene.blenrig_guide.character_fingers_obj.data.shape_keys.key_blocks['Weights_Transfer_Hands'].value = 0.0
+    except:
+        pass
+    try:
+        bpy.context.scene.blenrig_guide.character_head_obj.data.shape_keys.key_blocks['Weights_Transfer_Head'].value = 0.0
+    except:
+        pass
+
+    #Add Deform Modifiers to Character's head
+    bpy.context.view_layer.objects.active = bpy.context.scene.blenrig_guide.character_head_obj
+    active = bpy.context.active_object
+
+    #Armature
+    if check_mod_type('ARMATURE'):
+        pass
+    else:
+        mod = active.modifiers.new(name= "Armature",type= 'ARMATURE')
+        # set modifier properties
+        mod.object = bpy.context.scene.blenrig_guide.arm_obj
+        mod.use_deform_preserve_volume = True
+        mod.vertex_group = 'no_mdef'
+        mod.show_expanded = True
+        mod.show_in_editmode = True
+        mod.show_on_cage = True
+    #Mesh Deform
+    if check_mod_type('MESH_DEFORM'):
+        pass
+    else:
+        mod = active.modifiers.new(name= "MeshDeform",type= 'MESH_DEFORM')
+        # set modifier properties
+        mod.object = bpy.context.scene.blenrig_guide.mdef_cage_obj
+        mod.invert_vertex_group = True
+        mod.vertex_group = 'no_mdef'
+        mod.show_expanded = True
+        mod.show_in_editmode = True
+        mod.show_on_cage = True
+
+    #Corrective Smooth
+    if check_mod_type('CORRECTIVE_SMOOTH'):
+        pass
+    else:
+        mod = active.modifiers.new(name= "CorrectiveSmooth",type= 'CORRECTIVE_SMOOTH')
+        # set modifier properties
+        mod.smooth_type = 'SIMPLE'
+        mod.rest_source = 'ORCO'
+        mod.vertex_group = 'corrective_smooth'
+        mod.show_expanded = False
+
+    #Cheek Puffs
+    if check_mod_type_name('WARP', 'Cheek_Puff_L'):
+        pass
+    else:
+        mod = active.modifiers.new(name= "Cheek_Puff_L",type= 'WARP')
+        # set modifier properties
+        mod.object_from = bpy.context.scene.blenrig_guide.arm_obj
+        mod.bone_from = 'cheek_puff_mstr_L'
+        mod.object_to = bpy.context.scene.blenrig_guide.arm_obj
+        mod.bone_to = 'cheek_puff_ctrl_L'
+        mod.vertex_group = 'no_mdef'
+        mod.falloff_radius = bpy.context.scene.blenrig_guide.arm_obj.pose.bones["cheek_puff_ctrl_L"]["PUFF_RADIUS_L"]
+        mod.show_expanded = False
+
+    if check_mod_type_name('WARP', 'Cheek_Puff_R'):
+        pass
+    else:
+        mod = active.modifiers.new(name= "Cheek_Puff_R",type= 'WARP')
+        # set modifier properties
+        mod.object_from = bpy.context.scene.blenrig_guide.arm_obj
+        mod.bone_from = 'cheek_puff_mstr_R'
+        mod.object_to = bpy.context.scene.blenrig_guide.arm_obj
+        mod.bone_to = 'cheek_puff_ctrl_R'
+        mod.vertex_group = 'no_mdef'
+        mod.falloff_radius = bpy.context.scene.blenrig_guide.arm_obj.pose.bones["cheek_puff_ctrl_R"]["PUFF_RADIUS_R"]
+        mod.show_expanded = False
+
+    #Lattices
+    if check_mod_type_name('LATTICE', 'LATTICE_EYE_L'):
+        pass
+    else:
+        mod = active.modifiers.new(name= "LATTICE_EYE_L",type= 'LATTICE')
+        # set modifier properties
+        for ob in bpy.data.objects:
+            if ob.type == 'LATTICE':
+                if 'LATTICE_EYE_L' in ob.name:
+                    if ob.parent == bpy.context.scene.blenrig_guide.arm_obj:
+                        mod.object = ob
+        mod.vertex_group = 'lattice_eye_L'
+        mod.show_expanded = False
+
+    if check_mod_type_name('LATTICE', 'LATTICE_EYE_R'):
+        pass
+    else:
+        mod = active.modifiers.new(name= "LATTICE_EYE_R",type= 'LATTICE')
+        # set modifier properties
+        for ob in bpy.data.objects:
+            if ob.type == 'LATTICE':
+                if 'LATTICE_EYE_R' in ob.name:
+                    if ob.parent == bpy.context.scene.blenrig_guide.arm_obj:
+                        mod.object = ob
+        mod.vertex_group = 'lattice_eye_R'
+        mod.show_expanded = False
+
+    if check_mod_type_name('LATTICE', 'LATTICE_BROW'):
+        pass
+    else:
+        mod = active.modifiers.new(name= "LATTICE_BROW",type= 'LATTICE')
+        # set modifier properties
+        for ob in bpy.data.objects:
+            if ob.type == 'LATTICE':
+                if 'LATTICE_BROW' in ob.name:
+                    if ob.parent == bpy.context.scene.blenrig_guide.arm_obj:
+                        mod.object = ob
+        mod.vertex_group = 'lattice_brow'
+        mod.show_expanded = False
+
+    if check_mod_type_name('LATTICE', 'LATTICE_MOUTH'):
+        pass
+    else:
+        mod = active.modifiers.new(name= "LATTICE_MOUTH",type= 'LATTICE')
+        # set modifier properties
+        for ob in bpy.data.objects:
+            if ob.type == 'LATTICE':
+                if 'LATTICE_MOUTH' in ob.name:
+                    if ob.parent == bpy.context.scene.blenrig_guide.arm_obj:
+                        mod.object = ob
+        mod.vertex_group = 'lattice_mouth'
+        mod.show_expanded = False
+
+    if check_mod_type_name('LATTICE', 'LATTICE_HEAD'):
+        pass
+    else:
+        mod = active.modifiers.new(name= "LATTICE_HEAD",type= 'LATTICE')
+        # set modifier properties
+        for ob in bpy.data.objects:
+            if ob.type == 'LATTICE':
+                if 'LATTICE_HEAD' in ob.name:
+                    if ob.parent == bpy.context.scene.blenrig_guide.arm_obj:
+                        mod.object = ob
+        mod.vertex_group = 'lattice_head'
+        mod.show_expanded = False
+
+    #Subsurf
+    subsurf_mods = [mod for mod in active.modifiers if mod.type == 'SUBSURF']
+    if subsurf_mods:
+        active.modifiers.remove(subsurf_mods[-1])
+    mod = active.modifiers.new(name= "Subdivision",type= 'SUBSURF')
+    # set modifier properties
+    mod.subdivision_type = 'CATMULL_CLARK'
+    mod.levels = 0
+    mod.render_levels = 3
+    mod.show_expanded = True
 
     #Select Armature
+    deselect_all_objects(context)
+    bpy.context.scene.blenrig_guide.arm_obj.hide_viewport = False
     armature = bpy.context.scene.blenrig_guide.arm_obj
     armature.select_set(state=True)
     bpy.context.view_layer.objects.active = armature
@@ -234,16 +411,6 @@ def DT_Finish(operator, context):
     # Front View.
     set_view_perspective(context, False)
     set_viewpoint('FRONT')
-
-    #Turn off Tranfer Shapekeys on Character's Objects
-    try:
-        bpy.context.scene.blenrig_guide.character_fingers_obj.data.shape_keys.key_blocks['Weights_Transfer_Hands'].value = 0.0
-    except:
-        pass
-    try:
-        bpy.context.scene.blenrig_guide.character_head_obj.data.shape_keys.key_blocks['Weights_Transfer_Head'].value = 0.0
-    except:
-        pass
 
 #### END OF STEP ACTIONS ####
 #Property for action to be performed after steps
