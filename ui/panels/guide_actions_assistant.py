bdada37bbddfffd4aefe855b794f870f2742e1f2
jpbouza <jpbouza@hotmail.com>
2021-06-18 00:54:39 -0300

    Actions Guide

--- ui/panels/guide_actions_assistant.py
+++ ui/panels/guide_actions_assistant.py
@@ -24,13 +24,32 @@ class BLENRIG_PT_actions_guide(bpy.types.Panel):
         return obj or obj.type in valid_types
 
     def draw(self, context):
+        arm_obj_props = bpy.context.scene.blenrig_guide
         layout = self.layout
 
-        # if VIEW3D_OT_blenrig_guide_lattices.instance and bpy.context.scene.blenrig_guide.guide_current_step == 'MDEF_Select_Body_Objects':
-        #     steps = layout.column(align=True)
-        #     box_set = steps.box()
-        #     box_set.label(text='Define Object tha use Mesh Deform')
-        #     box_set.operator("blenrig.add_body_modifiers", text = 'Set Body Objects')
+        if VIEW3D_OT_blenrig_guide_actions.instance and bpy.context.scene.blenrig_guide.guide_current_step == 'ACTIONS_Select_Body_Objects':
+            steps = layout.column(align=True)
+            box_set = steps.box()
+            box_set.label(text='Define Object tha use Mesh Deform')
+            box_set.prop(arm_obj_props, "guide_mouth_corner_out")
+            box_set.prop(arm_obj_props, "guide_mouth_corner_in")
+            box_set.prop(arm_obj_props, "guide_mouth_corner_up")
+            box_set.prop(arm_obj_props, "guide_mouth_corner_down")
+            box_set.prop(arm_obj_props, "guide_mouth_corner_back")
+            box_set.prop(arm_obj_props, "guide_mouth_corner_forw")
+            box_set.prop(arm_obj_props, "guide_jaw_up")
+            box_set.prop(arm_obj_props, "guide_jaw_down")
+            box_set.prop(arm_obj_props, "guide_cheek_up")
+            box_set.prop(arm_obj_props, "guide_cheek_down")
+            box_set.prop(arm_obj_props, "guide_nose_forwn")
+            box_set.prop(arm_obj_props, "guide_mouth_forwn")
+            box_set.prop(arm_obj_props, "guide_chin_forwn")
+            box_set.prop(arm_obj_props, "guide_eyelid_up_up")
+            box_set.prop(arm_obj_props, "guide_eyelid_up_down")
+            box_set.prop(arm_obj_props, "guide_eyelid_low_down")
+            box_set.prop(arm_obj_props, "guide_eyelid_low_up")
+            box_set.prop(bpy.context.scene.blenrig_guide.arm_obj.data, "layers", index=27, text='Show Deformation Bones')
+
 
 
 
