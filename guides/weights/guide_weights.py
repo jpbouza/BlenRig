279a539d71e08d53da166176abc24b5aa90a1199
jpbouza <jpbouza@hotmail.com>
2021-06-30 01:12:15 -0300

    Weights Guide

--- guides/weights/guide_weights.py
+++ guides/weights/guide_weights.py
@@ -15,47 +15,8 @@ GUIDE_STEPS_WEIGHTS = (
             'EN': "Select all the objects that conform the character's body and press the Set Body Objects button",
             'ES': 'Si el personaje es simétrico, activa la opción X-Mirror'
             },
-        'accion': MDEF_Select_Body_Objects
+        'accion': WEIGHTS_Cage_Ankle
     },
-    #Edit the Mesh Deform Cage
-    {
-        'imagen': 'DT_Finish_A.jpg',
-        'titulo': {
-            'EN': 'Edit the Mesh Deform Cage',
-            'ES': 'Paso 1'
-            },
-        'texto': {
-            'EN': "Edit the Mesh Deform Cage so that it wraps around the charcter's mesh",
-            'ES': 'Si el personaje es simétrico, activa la opción X-Mirror'
-            },
-        'accion': MDEF_Edit_Mdef_Cage
-},
-    #Check Mesh Deform Binding
-    {
-        'imagen': 'DT_Finish_A.jpg',
-        'titulo': {
-            'EN': 'Check Mesh Deform Binding',
-            'ES': 'Paso 1'
-            },
-        'texto': {
-            'EN': "Press the Bind Mesh Deform Button. Move the character away and check that all of its vertices are moving along with the rig. Otherwise, press the Unbind button, Edit the cage and Bind again until all the vertices move with the rig",
-            'ES': 'Si el personaje es simétrico, activa la opción X-Mirror'
-            },
-        'accion': MDEF_Binding_Check
-},
-    #Mesh Deform Final Binding
-    {
-        'imagen': 'DT_Finish_A.jpg',
-        'titulo': {
-            'EN': 'Mesh Deform Final Binding',
-            'ES': 'Paso 1'
-            },
-        'texto': {
-            'EN': "If all vertices are moving along, press the Unbind button and then the Bind Mesh Deform(Final) Button. Go grab a cup of coffee because depending on how many objects you are binding, this can take long.",
-            'ES': 'Si el personaje es simétrico, activa la opción X-Mirror'
-            },
-        'accion': MDEF_Final_Binding
-}
 )
 
 
--- guides/weights/guide_weights_actions.py
+++ guides/weights/guide_weights_actions.py
@@ -14,16 +14,7 @@ def select_armature(operator, context):
     set_active_object(context, operator.arm_obj)
     set_mode('POSE')
 
-#### MDEF STEPS ####
-
-def MDEF_Select_Body_Objects(operator, context):
-    #Perform end of step action and set current step name
-    end_of_step_action(context)
-    bpy.context.scene.blenrig_guide.guide_current_step = 'MDEF_Select_Body_Objects'
-
-    #Set Mdef Cage
-    deselect_all_objects(context)
-
+def show_armature(context):
     #Armature for setting view
     bpy.context.scene.blenrig_guide.arm_obj.hide_viewport = False
 
@@ -34,34 +25,8 @@ def MDEF_Select_Body_Objects(operator, context):
     if context.mode != 'POSE':
         set_mode('POSE')
 
-    # Adjust view to Bones.
-    frame_bones(context, "head_str", "master")
-
-    #Set back Object Mode
-    if context.mode != 'OBJECT':
-        set_mode('OBJECT')
-
-    armature.hide_viewport = True
-
+def show_mdef_cage(context):
     deselect_all_objects(context)
-
-    # Front View.
-    set_view_perspective(context, False)
-    set_viewpoint('FRONT')
-
-    #Select Head object
-    try:
-        bpy.context.view_layer.objects.active = bpy.context.scene.blenrig_guide.character_head_obj
-    except:
-        pass
-
-def MDEF_Edit_Mdef_Cage(operator, context):
-    #Perform end of step action and set current step name
-    end_of_step_action(context)
-    bpy.context.scene.blenrig_guide.guide_current_step = 'MDEF_Edit_Mdef_Cage'
-
-    deselect_all_objects(context)
-
     # Show Mdef
     mdef_cage_objects = collect_cage()
     collect_cage()
@@ -71,95 +36,129 @@ def MDEF_Edit_Mdef_Cage(operator, context):
         set_active_object(context, ob)
         bpy.context.scene.blenrig_guide.mdef_cage_obj = ob
         bpy.context.scene.blenrig_guide.mdef_cage_obj.hide_viewport = False
-        set_mode('EDIT')
 
-def MDEF_Binding_Check(operator, context):
+def ball_joint_rotations(BONE, X, X_NEG, Y, Y_NEG, Z, Z_NEG, PROP_VALUE):
+    #Set Bone and Angles
+    guide_props = bpy.context.scene.blenrig_guide
+    guide_props.guide_transformation_bone = BONE
+    guide_props.guide_x_rotation = X
+    guide_props.guide_y_rotation = Y
+    guide_props.guide_z_rotation = Z
+    guide_props.guide_x_rotation_neg = X_NEG
+    guide_props.guide_y_rotation_neg = Y_NEG
+    guide_props.guide_z_rotation_neg = Z_NEG
+    #Reset rotation property
+    guide_props.guide_ball_joint_rotate = PROP_VALUE
+
+#### WEIGHTS STEPS ####
+
+def WEIGHTS_Cage_Ankle(operator, context):
     #Perform end of step action and set current step name
     end_of_step_action(context)
-    bpy.context.scene.blenrig_guide.guide_current_step = 'MDEF_Binding_Check'
+    bpy.context.scene.blenrig_guide.guide_current_step = 'WEIGHTS_Cage_Ankle'
 
-    #Set Mdef Cage
+    #Cage
+    show_mdef_cage(context)
+    mdef_cage = bpy.context.scene.blenrig_guide.mdef_cage_obj
+    set_active_vgroup('foot_def_L')
+
+    #Set Bone and Angles
+    ball_joint_rotations('sole_ctrl_L', 60, -60, 45, -45, 60, -60, 0)
+
     deselect_all_objects(context)
 
-    #Armature for setting view
-    bpy.context.scene.blenrig_guide.arm_obj.hide_viewport = False
-
-    #Select Armature
-    armature = bpy.context.scene.blenrig_guide.arm_obj
-    armature.select_set(state=True)
-    bpy.context.view_layer.objects.active = armature
-    if context.mode != 'POSE':
-        set_mode('POSE')
+    #Show Armature
+    show_armature(context)
 
     # Adjust view to Bones.
-    frame_bones(context, "head_str", "master")
-
-    #Set back Object Mode
-    if context.mode != 'OBJECT':
-        set_mode('OBJECT')
-
-    armature.hide_viewport = True
-
-    deselect_all_objects(context)
-
+    frame_bones(context, "foot_fk_L", "sole_pivot_point_L")
     # Front View.
     set_view_perspective(context, False)
-    set_viewpoint('FRONT')
+    set_viewpoint('RIGHT')
 
-    #Select Head object
-    try:
-        bpy.context.view_layer.objects.active = bpy.context.scene.blenrig_guide.character_head_obj
-    except:
-        pass
+    #Toggle Pose X-Mirror
+    toggle_pose_x_mirror(context, True)
 
+    bones = ('sole_ctrl_L', 'sole_ctrl_R')
 
+    unhide_all_bones(context)
+    select_all_pose_bones(context)
+    deselect_pose_bones(context, *bones)
+    hide_selected_pose_bones(context)
 
-def MDEF_Final_Binding(operator, context):
-    #Perform end of step action and set current step name
-    end_of_step_action(context)
-    bpy.context.scene.blenrig_guide.guide_current_step = 'MDEF_Final_Binding'
+    #Turn On Deformation Layer
+    on_layers = [27]
+    for l in on_layers:
+        bpy.context.object.data.layers[l] = True
 
-    #Set Mdef Cage
-    deselect_all_objects(context)
+    #Unhide Bones in Deformation Layer
+    hide_bones_in_layer(context, *on_layers, state=False)
+    deselect_all_pose_bones(context, invert=False)
 
-    #Armature for setting view
-    bpy.context.scene.blenrig_guide.arm_obj.hide_viewport = False
+    #Set Active Bone
+    select_pose_bone(context, 'sole_ctrl_L')
 
-    #Select Armature
-    armature = bpy.context.scene.blenrig_guide.arm_obj
-    armature.select_set(state=True)
-    bpy.context.view_layer.objects.active = armature
-    if context.mode != 'POSE':
-        set_mode('POSE')
+    #Set Weight Paint Mode
+    set_active_object(context, mdef_cage)
+    if context.mode != 'WEIGHT_PAINT':
+        set_mode('WEIGHT_PAINT')
 
-    # Adjust view to Bones.
-    frame_bones(context, "head_str", "master")
-
-    #Set back Object Mode
-    if context.mode != 'OBJECT':
-        set_mode('OBJECT')
-
-    armature.hide_viewport = True
-
-    deselect_all_objects(context)
-
-    # Front View.
-    set_view_perspective(context, False)
-    set_viewpoint('FRONT')
-
-    #Select Head object
-    try:
-        bpy.context.view_layer.objects.active = bpy.context.scene.blenrig_guide.character_head_obj
-    except:
-        pass
+    #Active VGroup
+    set_active_vgroup('foot_def_L')
 
 #### END OF STEP ACTIONS ####
+
+def actions_end_generic(context):
+    #Force Pose Position
+    bpy.context.active_object.data.pose_position = 'POSE'
+
+    #Ensure Symmetry
+    unhide_all_bones(context)
+    deselect_all_pose_bones(context)
+
+    #Left Side
+    for b in context.pose_object.data.bones:
+        if b.name.endswith('_L'):
+            b.select = True
+
+    if bpy.context.active_object.pose.use_mirror_x == True:
+        mirror_pose()
+    deselect_all_pose_bones(context)
+
+    #Clear Action and Transforms
+    clear_action()
+
+    #Reset Transforms
+    reset_all_bones_transforms()
+
+    #Toggle Pose X-Mirror
+    toggle_pose_x_mirror(context, False)
+
+    #Turn Layers on
+    off_layers = [27, 28]
+    for l in off_layers:
+        bpy.context.object.data.layers[l] = False
+
 #Property for action to be performed after steps
 def end_of_step_action(context):
-    current_step = bpy.context.scene.blenrig_guide.guide_current_step
-    if current_step == 'MDEF_Edit_Mdef_Cage':
-        #Set back Object Mode
-        if context.mode != 'OBJECT':
-            set_mode('OBJECT')
-        blenrig_temp_unlink()
-        bpy.context.scene.blenrig_guide.guide_current_step = ''
+    print ('kk')
+    # guide_props = bpy.context.scene.blenrig_guide
+    # current_step = bpy.context.scene.blenrig_guide.guide_current_step
+    # steps = ['ACTIONS_Fingers_Spread_X_Up', 'ACTIONS_Fingers_Spread_X_Down', 'ACTIONS_Fingers_Spread_Z_Out', 'ACTIONS_Fingers_Spread_Z_In',
+    # 'ACTIONS_Fingers_Curl_In', 'ACTIONS_Fingers_Curl_Out', 'ACTIONS_Hand_Close', 'ACTIONS_Hand_Open', 'ACTIONS_Breathing_in', 'ACTIONS_Breathing_Out',
+    # 'ACTIONS_Eyelids_Up_Up_Range', 'ACTIONS_Eyelids_Up_Up', 'ACTIONS_Eyelids_Up_Down_Range', 'ACTIONS_Eyelids_Up_Down_1', 'ACTIONS_Eyelids_Up_Down_2',
+    # 'ACTIONS_Eyelids_Low_Down_Range', 'ACTIONS_Eyelids_Low_Down', 'ACTIONS_Eyelids_Low_Up_Range', 'ACTIONS_Eyelids_Low_Up_1', 'ACTIONS_Eyelids_Low_Up_2',
+    # 'ACTIONS_Cheek_Up_Range', 'ACTIONS_Cheek_Up', 'ACTIONS_Cheek_Down_Range', 'ACTIONS_Cheek_Down', 'ACTIONS_Cheek_Frown', 'ACTIONS_Eyelids_Out', 'ACTIONS_Eyelids_In',
+    # 'ACTIONS_Jaw_Down_Range', 'ACTIONS_Jaw_Up_Range', 'ACTIONS_Jaw_Down', 'ACTIONS_Jaw_Up', 'ACTIONS_Mouth_Corner_Out_Range', 'ACTIONS_Mouth_Corner_Out', 'ACTIONS_Mouth_Corner_Up_Range', 'ACTIONS_Mouth_Corner_Up',
+    # 'ACTIONS_Mouth_Corner_Down_Range', 'ACTIONS_Mouth_Corner_Down', 'ACTIONS_Mouth_Corner_Back_Range', 'ACTIONS_Mouth_Corner_Back', 'ACTIONS_Mouth_Corner_Forw_Range', 'ACTIONS_Mouth_Corner_Forw',
+    # 'ACTIONS_Mouth_Corner_In_Range', 'ACTIONS_Mouth_Corner_In', 'ACTIONS_Mouth_Corner_Up_Out_Corrective', 'ACTIONS_Mouth_Corner_Down_Out_Corrective'
+    # 'ACTIONS_U_O_M_Range', 'ACTIONS_U', 'ACTIONS_O', 'ACTIONS_M', 'ACTIONS_U_Narrow_Corrective', 'ACTIONS_U_Thicker_Lips', 'ACTIONS_U_Thinner_Lips',
+    # 'ACTIONS_Mouth_Frown_Range', 'ACTIONS_Mouth_Frown', 'ACTIONS_Chin_Frown_Range','ACTIONS_Chin_Frown_Up', 'ACTIONS_Chin_Frown_Down',
+    # 'ACTIONS_Mouth_Corner_In_Zipper', 'ACTIONS_U_Zipper', 'ACTIONS_O_Zipper', 'ACTIONS_U_Narrow_Corrective_Zipper']
+    # for step in steps:
+    #     if current_step == step:
+    #         actions_end_generic(context)
+    #         bpy.context.scene.blenrig_guide.guide_current_step = ''
+    # if current_step == 'ACTIONS_Eyelids_Up_Up_Range':
+    #     #Enable Action Constraints
+    #     mute_constraints('Eyelid_Upper_Up', False)
