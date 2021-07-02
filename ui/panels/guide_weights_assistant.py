f326efab26d579a5727a8ca8aea56cf795f4ce75
jpbouza <jpbouza@hotmail.com>
2021-07-02 01:42:04 -0300

    WP Guide

--- ui/panels/guide_weights_assistant.py
+++ ui/panels/guide_weights_assistant.py
@@ -31,23 +31,31 @@ class BLENRIG_PT_weights_guide(bpy.types.Panel):
 
         if VIEW3D_OT_blenrig_guide_weights.instance:
             steps = layout.column(align=True)
-            box_set = steps.box()
-            box_set.label(text='Weight Painting Options')
-            box_set.operator("blenrig.toggle_weight_painting", text='Toggle Weight Painting').paint_object = 'mdef_cage'
-            mirror_row = box_set.row()
+            box_weight = steps.box()
+            box_weight.label(text='Weight Painting Options')
+            box_weight.operator("blenrig.toggle_weight_painting", text='Toggle Weight Painting').paint_object = 'mdef_cage'
+            box_weight.operator("blenrig.wp_joint_chain_up", text='>')
+            box_weight.operator("blenrig.wp_joint_chain_down", text='<')
+            box_weight.label(text=guide_props.guide_transformation_bone)
+            mirror_row = box_weight.row()
             mirror_row.prop(guide_props.arm_obj.pose, "use_mirror_x", text='X-Axis Mirror (Pose)')
             if active == 'WEIGHT_PAINT':
                 mirror_row.prop(guide_props.mdef_cage_obj.data, "use_mirror_vertex_groups")
                 mirror_row.prop(guide_props.mdef_cage_obj.data, "use_mirror_topology")
+            steps.separator()
 
         if VIEW3D_OT_blenrig_guide_weights.instance and bpy.context.scene.blenrig_guide.guide_current_step == 'WEIGHTS_Cage_Ankle':
-            box_set.label(text='Set Joint Transforms')
-            box_set.prop(guide_props, "guide_joint_rotate_X6", text='Ankle Pose')
+            box_pose = steps.box()
+            box_pose.label(text='Set Joint Transforms')
+            box_pose.prop(guide_props, "guide_joint_rotate_X6", text='Ankle Pose')
             if guide_props.guide_joint_rotate_X6 != 0:
-                box_set.label(text='Realistic Joints Parameters')
-                box_set.prop(p_bones["properties_leg_L"], 'realistic_joints_ankle_rot_L', text='Ankle Displacement')
-            if guide_props.guide_joint_rotate_X6 == 1:
-                row_props = box_set.row()
+                box_pose.label(text='Realistic Joints Parameters')
+                box_pose.prop(p_bones["properties_leg_L"], 'realistic_joints_ankle_rot_L', text='Ankle Displacement')
+            if guide_props.guide_joint_rotate_X6 == 5 or guide_props.guide_joint_rotate_X6 == 6 :
+                box_pose.label(text='Shin Twist Rate')
+                box_pose.prop(p_bones["properties_leg_L"], '["twist_rate_shin_L"]', text='Twist Rate')
+            if guide_props.guide_joint_rotate_X6 == 2:
+                row_props = box_pose.row()
                 col_R = row_props.column()
                 col_L = row_props.column()
                 col_R.label(text='Volume Preservation: Instep')
@@ -57,12 +65,20 @@ class BLENRIG_PT_weights_guide(bpy.types.Panel):
                     col_L.prop(p_bones['instep_fix_L'].constraints['Instep_VP_Up_L_NOREP'], 'to_min_z', text="Vertical", toggle=True)
                 except:
                     pass
+            if guide_props.guide_joint_rotate_X6 == 1:
+                row_props = box_pose.row()
+                col_R = row_props.column()
+                col_L = row_props.column()
+                col_R.label(text='Volume Preservation: Ankle')
+                col_L.label(text=' ')
+                try:
+                    col_R.prop(p_bones['ankle_fix_L'].constraints['Ankle_VP_Down_L_NOREP'], 'to_max_y', text="Outwards", toggle=True)
+                    col_L.prop(p_bones['ankle_fix_L'].constraints['Ankle_VP_Down_L_NOREP'], 'to_max_z', text="Vertical", toggle=True)
+                except:
+                    pass
             if guide_props.guide_joint_rotate_X6 != 0:
-                box_set.separator()
-                box_set.operator("blenrig._mirror_vp_rj_values", text='Mirror Values')
-
-
-
+                box_pose.separator()
+                box_pose.operator("blenrig.mirror_vp_rj_values", text='Mirror Values')
 
 
 
