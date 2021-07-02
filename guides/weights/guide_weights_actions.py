910aa0d82bfc449d892b5d1f19612b32b5f55a53
jpbouza <jpbouza@hotmail.com>
2021-07-02 01:41:39 -0300

    WP Guide

--- guides/weights/guide_weights_actions.py
+++ guides/weights/guide_weights_actions.py
@@ -54,34 +54,36 @@ def weight_step(operator, context, step_name, wp_obj,
 joint_type, joint_parameters, rot_order,
 frame_bone_1, frame_bone_2, view,
 bone_list, layers_list, active_bone, wp_active_group, mode):
+
+    guide_props = bpy.context.scene.blenrig_guide
     #Perform end of step action and set current step name
     end_of_step_action(context)
-    bpy.context.scene.blenrig_guide.guide_current_step = step_name
+    guide_props.guide_current_step = step_name
 
     #Weight Painting Object
     #Cage
     if wp_obj == 'mdef_cage':
         show_mdef_cage(context)
-        paint_obj = bpy.context.scene.blenrig_guide.mdef_cage_obj
+        paint_obj = guide_props.mdef_cage_obj
         set_active_object(context, paint_obj)
     #Hands
     elif wp_obj == 'hands':
-        paint_obj = bpy.context.scene.blenrig_guide.character_hands_obj
+        paint_obj = guide_props.character_hands_obj
         set_active_object(context, paint_obj)
     #Toes
     elif wp_obj == 'toes':
-        paint_obj = bpy.context.scene.blenrig_guide.character_toes_obj
+        paint_obj = guide_props.character_toes_obj
         set_active_object(context, paint_obj)
     #Head
     elif wp_obj == 'hands':
-        paint_obj = bpy.context.scene.blenrig_guide.character_head_obj
+        paint_obj = guide_props.character_head_obj
         set_active_object(context, paint_obj)
 
     #Active VGroup
     set_active_vgroup(wp_active_group)
 
     #Set Bone and Angles
-    bpy.context.scene.blenrig_guide.guide_rotation_order = rot_order
+    guide_props.guide_rotation_order = rot_order
     param_list = joint_parameters
     if joint_type == 'ball':
         joint_x6_rotations(*param_list)
@@ -96,9 +98,6 @@ bone_list, layers_list, active_bone, wp_active_group, mode):
     # Front View.
     set_view_perspective(context, False)
     set_viewpoint(view)
-
-    #Toggle Pose X-Mirror
-    toggle_pose_x_mirror(context, True)
 
     bones = bone_list
 
@@ -137,66 +136,27 @@ bone_list, layers_list, active_bone, wp_active_group, mode):
 
 def WEIGHTS_Cage_Ankle(operator, context):
     weight_step(operator, context, 'WEIGHTS_Cage_Ankle', 'mdef_cage',
-    'ball', ['sole_ctrl_L', 60, -60, 45, -45, 60, -60, 1], 'XYZ',
+    'ball', ['foot_fk_L', 60, -60, 45, -45, 60, -60, 1], 'XZY',
     'foot_fk_L', 'sole_pivot_point_L', 'RIGHT',
-    ['sole_ctrl_L', 'sole_ctrl_R'], [27], 'sole_ctrl_L', 'foot_def_L', 'mdef_mesh')
+    ['foot_fk_L', 'foot_fk_R'], [27], 'foot_fk_L', 'foot_def_L', 'mdef_mesh',)
 
+    #Set Rig Control Properties
+    bpy.ops.snap.leg_ik_to_fk_l()
+    bpy.ops.snap.leg_ik_to_fk_r()
 
-    # #Perform end of step action and set current step name
-    # end_of_step_action(context)
-    # bpy.context.scene.blenrig_guide.guide_current_step = 'WEIGHTS_Cage_Ankle'
+    joint_list = ['foot_fk_L', 'foot_def_L', 'foot_def_R']
 
-    # #Cage
-    # show_mdef_cage(context)
-    # mdef_cage = bpy.context.scene.blenrig_guide.mdef_cage_obj
+    #Clear List
+    bpy.context.scene.blenrig_joint_chain_list.clear()
 
-    # #Set Bone and Angles
-    # joint_x6_rotations('sole_ctrl_L', 60, -60, 45, -45, 60, -60, 1)
-
-    # deselect_all_objects(context)
-
-    # #Show Armature
-    # show_armature(context)
-
-    # # Adjust view to Bones.
-    # frame_bones(context, "foot_fk_L", "sole_pivot_point_L")
-    # # Front View.
-    # set_view_perspective(context, False)
-    # set_viewpoint('RIGHT')
-
-    # #Toggle Pose X-Mirror
-    # toggle_pose_x_mirror(context, True)
-
-    # bones = ('sole_ctrl_L', 'sole_ctrl_R')
-
-    # unhide_all_bones(context)
-    # select_all_pose_bones(context)
-    # deselect_pose_bones(context, *bones)
-    # hide_selected_pose_bones(context)
-
-    # #Turn On Deformation Layer
-    # on_layers = [27]
-    # for l in on_layers:
-    #     bpy.context.object.data.layers[l] = True
-
-    # #Unhide Bones in Deformation Layer
-    # hide_bones_in_layer(context, *on_layers, state=False)
-    # deselect_all_pose_bones(context, invert=False)
-
-    # #Set Active Bone
-    # select_pose_bone(context, 'sole_ctrl_L')
-
-    # #Set Weight Paint Mode
-    # set_active_object(context, mdef_cage)
-    # if context.mode != 'WEIGHT_PAINT':
-    #     set_mode('WEIGHT_PAINT')
-
-    # #Active VGroup
-    # set_active_vgroup('foot_def_L')
+    #Define Body Objects
+    for b in joint_list:
+        add_item = bpy.context.scene.blenrig_joint_chain_list.add()
+        add_item.joint = b
 
 #### END OF STEP ACTIONS ####
 
-def actions_end_generic(context):
+def weights_end_generic(context):
     #Force Pose Position
     bpy.context.active_object.data.pose_position = 'POSE'
 
