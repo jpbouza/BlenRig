231b9113862a8ba9cc67bae15205be8d75773cac
jpbouza <jpbouza@hotmail.com>
2021-10-30 01:34:23 -0300

    Lattices Images

--- guides/lattices/guide_lattices.py
+++ guides/lattices/guide_lattices.py
@@ -6,7 +6,7 @@ images_dir = join(dirname(__file__), 'images')
 GUIDE_STEPS_LATTICES = (
     #Introduction
     {
-        'imagen': 'DT_Finish_A.jpg',
+        'imagen': 'LATTICES_Intro.jpg',
         'titulo': {
             'EN': 'Introduction',
             'ES': 'Introducción'
@@ -19,72 +19,72 @@ GUIDE_STEPS_LATTICES = (
     },
     #Edit Body Lattice
     {
-        'imagen': 'DT_Finish_A.jpg',
+        'imagen': ('LATTICES_Adjust_Body_A.jpg', 'LATTICES_Adjust_Body_B.jpg', 'LATTICES_Position.jpg'),
         'titulo': {
             'EN': 'Edit Body Lattice',
             'ES': 'Edita el Lattice del cuerpo'
             },
         'texto': {
-            'EN': "Adjust the Body Lattice, press the 'Edit Lattice Position' to move the lattice, then press the 'Apply Lattice Position' button.",
+            'EN': "Adjust the Body Lattice. Press the 'Edit Lattice Position' to move the lattice, then press the 'Apply Lattice Position' button.",
             'ES': "Ajuste el Lattice del cuerpo, pulsa el botón 'Edit Lattice Position' para mover el lattice, luego pulse el botón 'Apply Lattice Position'."
             },
         'accion': LATTICES_Adjust_Body
     },
     #Edit Head Lattice
     {
-        'imagen': 'DT_Finish_A.jpg',
+        'imagen': ('LATTICES_Adjust_Head_A.jpg', 'LATTICES_Adjust_Head_B.jpg', 'LATTICES_Position.jpg'),
         'titulo': {
             'EN': 'Edit Head Lattice',
             'ES': 'Edita el Lattice de la cabeza'
             },
         'texto': {
-            'EN': "Adjust the Head Lattice, press the 'Edit Lattice Position' to move the lattice, then press the 'Apply Lattice Position' button.",
+            'EN': "Adjust the Head Lattice. Press the 'Edit Lattice Position' to move the lattice, then press the 'Apply Lattice Position' button.",
             'ES': "Ajusta el Lattice de la cabeza, pulsa el botón 'Edit Lattice Position' para mover el lattice, luego pulse el botón 'Apply Lattice Position'."
             },
         'accion': LATTICES_Adjust_Head
     },
     #Edit Brow Lattice
     {
-        'imagen': 'DT_Finish_A.jpg',
+        'imagen': ('LATTICES_Adjust_Brow_A.jpg', 'LATTICES_Adjust_Brow_B.jpg', 'LATTICES_Position.jpg'),
         'titulo': {
             'EN': 'Edit Brow Lattice',
             'ES': 'Edita el Lattice de las cejas'
             },
         'texto': {
-            'EN': "Adjust the Brow Lattice, press the 'Edit Lattice Position' to move the lattice, then press the 'Apply Lattice Position' button.",
+            'EN': "Adjust the Brow Lattice. Press the 'Edit Lattice Position' to move the lattice, then press the 'Apply Lattice Position' button.",
             'ES': "Ajusta el Lattice de las cejas, pulsa el botón 'Edit Lattice Position' para mover el lattice, luego pulse el botón 'Apply Lattice Position'."
             },
         'accion': LATTICES_Adjust_Brow
     },
     #Edit Mouth Lattice
     {
-        'imagen': 'DT_Finish_A.jpg',
+        'imagen': ('LATTICES_Adjust_Mouth_A.jpg', 'LATTICES_Adjust_Mouth_B.jpg', 'LATTICES_Position.jpg'),
         'titulo': {
             'EN': 'Edit Mouth Lattice',
             'ES': 'Edita el Lattice de la boca'
             },
         'texto': {
-            'EN': "Adjust the Mouth Lattice, press the 'Edit Lattice Position' to move the lattice, then press the 'Apply Lattice Position' button.",
+            'EN': "Adjust the Mouth Lattice. Press the 'Edit Lattice Position' to move the lattice, then press the 'Apply Lattice Position' button.",
             'ES': "Ajusta el Lattice de la boca, pulsa el botón 'Edit Lattice Position' para mover el lattice, luego pulse el botón 'Apply Lattice Position'."
             },
         'accion': LATTICES_Adjust_Mouth
     },
     #Edit Eyes Lattice
     {
-        'imagen': 'DT_Finish_A.jpg',
+        'imagen': ('LATTICES_Adjust_Eyes_A.jpg', 'LATTICES_Adjust_Eyes_B.jpg', 'LATTICES_Adjust_Eyes_C.jpg'),
         'titulo': {
             'EN': 'Edit Eyes Lattices',
             'ES': 'Edita el Lattice de los ojos'
             },
         'texto': {
-            'EN': "Adjust the Lattice of the Left Eye, press the 'Edit Lattice Position' to move the lattice, then press the 'Apply Lattice Position' button. Finally Mirror the position to the Right Eye Lattice with the 'Mirror Lattice Transforms' button",
+            'EN': "Adjust the Lattice of the Left Eye. Press the 'Edit Lattice Position' to move the lattice, then press the 'Apply Lattice Position' button. Finally Mirror the position to the Right Eye Lattice with the 'Mirror Lattice Transforms' button",
             'ES': "Edita el Lattice de los ojos, pulsa el botón 'Edit Lattice Position' para mover el lattice, luego pulse el botón 'Apply Lattice Position'."
             },
         'accion': LATTICES_Adjust_Eyes
     },
     #Lattices Finish
     {
-        'imagen': 'DT_Finish_A.jpg',
+        'imagen': 'LATTICES_Finish.jpg',
         'titulo': {
             'EN': 'Lattices Finish',
             'ES': 'Edita el Lattice del cuerpo'
